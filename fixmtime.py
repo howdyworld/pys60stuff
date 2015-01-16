1d2d90462bd441340e4909ac79b11fcc78849e51
howdyworld <howdy.earth@gmail.com>
2015-01-16 19:50:30 +0200

    search .py files only in std python folder

--- fixmtime.py
+++ fixmtime.py
@@ -3,7 +3,7 @@ import os
 import time
 import re
 
-for root, dirs, files in os.walk("E:\\"):
+for root, dirs, files in os.walk("E:\\Data\\python"):
   for filename in files:
     if (re.search('\.py$',filename)!=None) and (os.stat(root + '\\' + filename).st_mtime==-1.0):
       print "fixed time on %s\\%s" % (root, filename)
